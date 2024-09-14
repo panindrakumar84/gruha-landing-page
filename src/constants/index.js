d9893eee994d0d2b7cc28753d3f882f0159538e0
panindrakumar84 <bphani678@gmail.com>
2024-09-14 21:46:36 +0530

    testimonial section first commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,7 @@
-
+import customer1 from "../assets/user-1.png"
+import customer2 from "../assets/user-2.png"
+import customer3 from "../assets/user-3.png"
+import customer4 from "../assets/user-4.png"
 
 export const navLinks = [
     {href:"#home",label:"Home"},
@@ -7,4 +10,31 @@ export const navLinks = [
     {href:"#pricing",label:"Pricing"},
     {href:"#contact-us",label:"Contact"},
     
+]
+
+export const reviews = [
+    {
+        imgUrl:customer1,
+        name: "Morich Brown",
+        address: "California, USA",
+        review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit pariatur exercitationem !"
+    },
+    {
+        imgUrl: customer2,
+        name: "Panindra Kumar",
+        address: "Florida, USA",
+        review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit pariatur exercitationem a perferendis!"
+    },
+    {
+        imgUrl:customer3,
+        name: "Lota Mongeskar",
+        address: "Texas USA",
+        review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit pariatur exercitationem a dolor!"
+    },
+    {
+        imgUrl: customer4,
+        name:"Jaswanth",
+        address: "California",
+        review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit pariatur exercitationem perferendis dolor!"
+    }
 ]
---
+++ src/sections/Testimonials.jsx
@@ -0,0 +1,69 @@
+import React, {  useState } from 'react'
+import TestimonialCard from '../components/TestimonialCard'
+import { reviews } from '../constants'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import { ArrowLeft, ArrowRight } from 'lucide-react';
+
+const Testimonials = () => {
+
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    initialSlide: 1,
+   
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
+ 
+  return (
+
+    <section className='max-container flex justify-center lg:padding-x'>
+    <div className='w-full py-4 px-2 md:px-10'>
+      
+      <Slider {...settings}>
+             {
+            reviews.map((item,index)=>(
+             
+                <TestimonialCard key={index}  image={item.imgUrl}
+                name={item.name}
+                address={item.address}
+                review={item.review}/>              
+            ))
+          }
+        </Slider>
+      
+    </div>
+    </section>
+  )
+}
+
+export default Testimonials
